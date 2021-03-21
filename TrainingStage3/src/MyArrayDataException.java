43d7f3d4e4cec4b264d3a255a96b6ec909c97d9c
BalakhninMI <BalakhninMI@gmail.com>
2021-03-21 23:53:49 +0800

    Практическое задание №2

---
+++ TrainingStage3/src/MyArrayDataException.java
@@ -0,0 +1,20 @@
+import static java.lang.String.*;
+
+public class MyArrayDataException extends RuntimeException {
+    private int row;
+    private int col;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public MyArrayDataException(String message, int row, int col) {
+        super(message);
+        this.row = row;
+        this.col = col;
+    }
+}
