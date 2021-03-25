d7ef851f17d607f30cb8ca60d8f683bb5c87e951
BalakhninMI <BalakhninMI@gmail.com>
2021-03-26 00:16:50 +0800

    Практическая часть урок №3.

---
+++ TrainingStage_3/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Wordsheet wh = new Wordsheet();
+        wh.setList(Arrays.asList("АБАЗИНЫ", "АББАТ", "АББАТИСА", "БАБОЧКА", "БАБУШИ", "БАБУШКА", "ВАГРАНКА", "ВАЖЕНКА", "ВАЖНЕЦКИЙ", "ГАДЮКА", "ГАДЮШНИК", "ГАЕР", "ТАВРЕНЫЙ", "ТАВРИТЬ", "ТАВРО", "АББАТИСА", "БАБУШКА", "ВАЖЕНКА", "ГАДЮКА", "ГАЕР", "ТАВРЕНЫЙ", "ТАВРО", "БАБУШИ", "БАБУШКА", "ВАЖЕНКА", "ГАДЮШНИК", "ТАВРИТЬ", "ГАДЮКА", "ТАВРЕНЫЙ", "АББАТ", "БАБУШКА", "ГАДЮКА", "ГАЕР", "ТАВРО", "ВАЖЕНКА", "ГАДЮКА", "БАБУШИ", "ВАЖЕНКА", "ГАДЮШНИК", "АББАТ"));
+//        wh.setList(Arrays.asList("АБАЗИНЫ", "АББАТ", "АББАТИСА", "БАБОЧКА","АББАТ"));
+//        System.out.println(Collections.unmodifiableList(wh.getList()));
+//        System.out.println("");
+//        System.out.println(wh.getUniqueWords());
+//        System.out.println(wh.getCountUniqueWords());
+    }
+}
